47a4d94ef852f91dbc2270102b3c97ffdb786b08
anecula <andra.easypost@gmail.com>
2018-01-10 00:13:49 +0200

    1

---
+++ views/public/js/global.js
@@ -0,0 +1,90 @@
+
+/*
+ * Book Leave request pop-up window.
+ *
+ * */
+$(document).ready(function(){
+  /*
+   *  When FROM field in New absense form chnages: update TO one if necessary
+   */
+  $('input.book-leave-from-input').on('change', function(e){
+    e.stopPropagation();
+
+    var from_date = $('input.book-leave-from-input').datepicker('getDate');
+
+    if ( ! from_date ) {
+      // no new value for FROM part, do nothing
+      console.log('No from date');
+      return;
+    }
+
+    var to_date = $('input.book-leave-to-input').datepicker('getDate');
+
+    if ( ! to_date || ( to_date && to_date.getTime() < from_date.getTime() )) {
+      $('input.book-leave-to-input').datepicker('setDate', $('input.book-leave-from-input').datepicker('getFormattedDate'));
+    }
+  });
+});
+
+
+/*
+ * Bootstrap-datepicker
+ *
+ * */
+!function(a){
+  a.fn.datepicker.dates["en-GB"] = {
+    days : [
+      "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"
+    ],
+    daysShort : [
+      "Sun","Mon","Tue","Wed","Thu","Fri","Sat"
+    ],
+    daysMin : [
+      "Su","Mo","Tu","We","Th","Fr","Sa"
+    ],
+    months : [
+      "January","February","March","April","May","June","July","August","September","October","November","December"
+    ],
+    monthsShort : [
+      "Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"
+    ],
+    today       : "Today",
+    monthsTitle : "Months",
+    clear       : "Clear",
+    weekStart   : 1,
+    format      : "dd/mm/yyyy"
+  }
+}(jQuery);
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
+/*
+ * This is handler for invocation of "add secondary supervisors" modal
+ *
+ * */
+
+$('#add_secondary_supervisers_modal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget),
+      department_name = button.data('department_name'),
+      department_id = button.data('department_id');
+
+  var modal = $(this);
+
+  modal.find('.modal-title strong').text(department_name);
+
+  // Make modal window to be no hiegher then window and its content
+  // scrollable
+  $('.modal .modal-body').css('overflow-y', 'auto');
+  $('.modal .modal-body').css('max-height', $(window).height() * 0.7);
+
+  $(this).find(".modal-body")
+    // Show "loading" icon while content of modal is loaded
+    .html('<p class="text-center"><i class="fa fa-refresh fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span></p>')
+    .load('/settings/departments/available-supervisors/'+department_id+'/');
+});
